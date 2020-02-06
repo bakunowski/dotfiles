257b4cc29c11fc6e169906cb90e7c3eb4f7a5f97
Karol <bakunowskikarol@protonmail.com>
2020-02-06 23:14:03 +0000

    status font changes

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Mono:size=8", "RobotoMono Nerd Font:size=9" };
+static const char *fonts[]          = { "SF Mono:size=10", "RobotoMono Nerd Font:size=10" };
 static const char dmenufont[]       = "RobotoMono Nerd Font:size=10";
 static const char col_gray1[]       = "#1d1f21";
 static const char col_gray2[]       = "#444444";

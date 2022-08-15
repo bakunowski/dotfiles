dde739f90e4e96e9bf1f0bfaee97a40f40dfae6d
Karol Bakunowski <bakunowskikarol@protonmail.com>
2022-08-15 23:46:59 +0100

    Use rasmus colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,39 +1,39 @@
-vim.o.clipboard = 'unnamedplus'		-- Yank to system cliboard
-vim.o.number = true								-- Show current line number
-vim.o.relativenumber = true				-- Show relative line numbers
-vim.g.mapleader = [[ ]]						-- Set leader to space
+vim.o.clipboard = 'unnamedplus' -- Yank to system cliboard
+vim.o.number = true -- Show current line number
+vim.o.relativenumber = true -- Show relative line numbers
+vim.g.mapleader = [[ ]] -- Set leader to space
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.expandtab = true						-- Use spaces instead of <Tab>
-vim.o.softtabstop = 4							-- Soft tab width in spaces
-vim.o.tabstop = 4									-- Tab width in spaces
-vim.o.shiftwidth = 4							-- Amount of spaces when shifting
-vim.o.termguicolors = true				-- Use 24-bit RGB color in terminal
-vim.o.mouse = 'a'									-- Enable mouse support
-vim.o.laststatus = 3							-- Global status line
-vim.o.list = true									-- Always show invisible characters
-vim.o.listchars = 'nbsp:¬,tab:	,trail:·,extends:>' -- Set the characters for the invisibles
+vim.o.expandtab = true -- Use spaces instead of <Tab>
+vim.o.softtabstop = 4 -- Soft tab width in spaces
+vim.o.tabstop = 4 -- Tab width in spaces
+vim.o.shiftwidth = 4 -- Amount of spaces when shifting
+vim.o.termguicolors = true -- Use 24-bit RGB color in terminal
+vim.o.mouse = 'a' -- Enable mouse support
+vim.o.laststatus = 3 -- Global status line
+vim.o.list = true -- Always show invisible characters
+vim.o.listchars = 'nbsp:¬,tab:  ,trail:·,extends:>' -- Set the characters for the invisibles
 
-vim.o.fixendofline = false				-- If there isn't an empty line at EOF don't insert it
-vim.o.hidden = true								-- Allow buffers to be backgrounded without being saved
-vim.o.updatetime = 100						-- cursor hold trigger after 100ms
-vim.o.colorcolumn = '80'					-- Highlight 80 character limit
-vim.o.signcolumn = 'auto'					-- How to display gitgutter signs next to numbers column
-vim.o.showmode = false						-- Don't show current mode in status line
-vim.o.scroll = 10									-- Number of lines to scroll with CTRL-U and CTRL-D commands
-vim.o.pumheight = 10							--	Maximum number of items to show in the popup menu
-vim.o.cmdheight=0                 -- Don't show empty command line if not in use
-vim.o.winbar='%=%f%m%='
+vim.o.fixendofline = false -- If there isn't an empty line at EOF don't insert it
+vim.o.hidden = true -- Allow buffers to be backgrounded without being saved
+vim.o.updatetime = 100 -- cursor hold trigger after 100ms
+vim.o.colorcolumn = '80' -- Highlight 80 character limit
+vim.o.signcolumn = 'auto' -- How to display gitgutter signs next to numbers column
+vim.o.showmode = false -- Don't show current mode in status line
+vim.o.scroll = 10 -- Number of lines to scroll with CTRL-U and CTRL-D commands
+vim.o.pumheight = 10 -- Maximum number of items to show in the popup menu
+vim.o.cmdheight = 0 -- Don't show empty command line if not in use
+vim.o.winbar = '%=%f%m%='
 vim.o.linebreak = true
 
 -- Tab is 2 spaces in these files:
-vim.cmd[[autocmd FileType yaml setlocal ts=2 sw=2]]
-vim.cmd[[autocmd FileType helm setlocal ts=2 sw=2]]
-vim.cmd[[autocmd FileType lua setlocal ts=2 sw=2]]
-vim.cmd[[autocmd FileType terraform setlocal ts=2 sw=2]]
+vim.cmd [[autocmd FileType yaml setlocal ts=2 sw=2]]
+vim.cmd [[autocmd FileType helm setlocal ts=2 sw=2]]
+vim.cmd [[autocmd FileType lua setlocal ts=2 sw=2]]
+vim.cmd [[autocmd FileType terraform setlocal ts=2 sw=2]]
 
 -- Show indent lines only in these files
-vim.cmd[[ let g:indent_blankline_filetype = ['yaml'] ]]
+vim.cmd [[ let g:indent_blankline_filetype = ['yaml'] ]]
 
 -- Lua library
 require('mappings')
@@ -56,13 +56,14 @@ require('telescope').setup({
 require('config.telescope')
 require('config.treesitter')
 require('nvim-autopairs').setup({
-	fast_wrap = {
-		map = '<C-e>',
-	},
+  fast_wrap = {
+    map = '<C-e>',
+  },
 })
 require('lsp')
 
-vim.cmd[[ colorscheme zenwritten ]]
+-- vim.cmd [[ set bg=light ]]
+vim.cmd [[ colorscheme rasmus ]]
 
 --command to get syntax highlighting in a new buffer
 --so $VIMRUNTIME/syntax/hitest.vim
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,9 @@
 -- Bootstrap installing packer on a new machine
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-	Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  Packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+    install_path })
 end
 
 -- Compile whenever changes are made to this file
@@ -14,94 +15,91 @@ vim.cmd([[
 ]])
 
 return require('packer').startup(function(use)
-	-- Manage yourself pls :)
-	use 'wbthomason/packer.nvim'
+  -- Manage yourself pls :)
+  use 'wbthomason/packer.nvim'
 
-	-- Completion
-	use {
-			'hrsh7th/nvim-cmp',
-			requires = {
-					{ 'hrsh7th/vim-vsnip', after = 'nvim-cmp' },
-					{ 'hrsh7th/cmp-vsnip', after = 'nvim-cmp' },
-					{ 'hrsh7th/cmp-nvim-lsp' },
-					{ 'hrsh7th/cmp-nvim-lsp-signature-help', after = 'nvim-cmp' },
-					{ 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
-					{ 'hrsh7th/cmp-path', after = 'nvim-cmp' },
-					{ 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
-					{ 'onsails/lspkind-nvim', after = 'nvim-cmp', },
-			},
-			config = function()
-				require('config.cmp')
-			end,
-			event = 'InsertEnter *'
-	}
+  -- Completion
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      { 'hrsh7th/vim-vsnip', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-vsnip', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lsp-signature-help', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
+      { 'onsails/lspkind-nvim', after = 'nvim-cmp', },
+    },
+    config = function()
+      require('config.cmp')
+    end,
+    event = 'InsertEnter *'
+  }
 
-	-- Complete my brackets!
-	use 'windwp/nvim-autopairs'
+  -- Complete my brackets!
+  use 'windwp/nvim-autopairs'
 
-	use 'tpope/vim-surround'
+  use 'tpope/vim-surround'
 
-	-- Comments with a keybinding
-	use {
-			'numToStr/Comment.nvim',
-			config = function()
-					require('Comment').setup()
-			end
-	}
+  -- Comments with a keybinding
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
 
-	-- Classic TJ
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  -- Classic TJ
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-	-- Easy native LSP setup
-	use 'neovim/nvim-lspconfig'
+  -- Easy native LSP setup
+  use 'neovim/nvim-lspconfig'
 
-	-- Better syntax highlighting
-	use {
-			'nvim-treesitter/nvim-treesitter',
-			requires = {
-					'nvim-treesitter/playground',
-					cmd = 'TSHighlightCapturesUnderCursor'
-			},
-			run = ':TSUpdate',
-	}
+  -- Better syntax highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      'nvim-treesitter/playground',
+      cmd = 'TSHighlightCapturesUnderCursor'
+    },
+    run = ':TSUpdate',
+  }
 
-	-- Go tests
-	use { 'buoto/gotests-vim', ft = 'go' }
+  -- Go tests
+  use { 'buoto/gotests-vim', ft = 'go' }
 
-	use 'towolf/vim-helm'
+  use 'towolf/vim-helm'
 
-	-- Git
-	use {
-			{
-					'tpope/vim-fugitive',
-					cmd = { 'G', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gdiff' },
-			},
-			{
-					'lewis6991/gitsigns.nvim',
-					requires = { 'nvim-lua/plenary.nvim' },
-					config = function() require('gitsigns').setup() end,
-			}
-	}
+  -- Git
+  use {
+    {
+      'tpope/vim-fugitive',
+      cmd = { 'G', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gdiff' },
+    },
+    {
+      'lewis6991/gitsigns.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = function() require('gitsigns').setup() end,
+    }
+  }
 
-	-- File tree view
-	use {
-			'kyazdani42/nvim-tree.lua',
-			requires = {
-				'kyazdani42/nvim-web-devicons', -- optional, for file icon
-			},
-			config = function() require'nvim-tree'.setup {} end,
-			cmd = 'NvimTreeToggle'
-	}
+  -- File tree view
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require 'nvim-tree'.setup {} end,
+    cmd = 'NvimTreeToggle'
+  }
 
-	-- And now make everything pretty
-	use {
-			"mcchrish/zenbones.nvim",
-			requires = "rktjmp/lush.nvim"
-	}
+  -- And now make everything pretty
+  use 'bakunowski/rasmus'
 
   use 'stevearc/dressing.nvim'
-	use 'lukas-reineke/indent-blankline.nvim'
+  use 'lukas-reineke/indent-blankline.nvim'
 end)
